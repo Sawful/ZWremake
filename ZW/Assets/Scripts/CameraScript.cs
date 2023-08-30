767f32b1a0322501740aac98a4c08a7b6cf26987
Colyn Co <colynco2@gmail.com>
2023-08-30 16:05:23 +0200

    Smooth Zoom done

--- ZW/Assets/Scripts/CameraScript.cs
+++ ZW/Assets/Scripts/CameraScript.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-
-    private float minFov = 30f;
-    private float maxFov = 120f;
-    private float sensitivity = 20f;
+    [SerializeField] private float fov;
+    [SerializeField] private float minFov = 30f;
+    [SerializeField] private float maxFov = 120f;
+    [SerializeField] private float sensitivity = 20f;
+    [SerializeField] private float velocity = 0F;
+    [SerializeField] private float smoothTime = 0.1F;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fov = Camera.main.fieldOfView;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float fov = Camera.main.fieldOfView;
-        fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity * -1;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        fov -= scroll * sensitivity;
         fov = Mathf.Clamp(fov, minFov, maxFov);
-        Camera.main.fieldOfView = fov;
+        Camera.main.fieldOfView = Mathf.SmoothDamp(Camera.main.fieldOfView, fov, ref velocity, smoothTime) ;
     }
 
     // FixedUpdate is Update for physics
@@ -32,28 +34,28 @@ public class CameraScript : MonoBehaviour
         {
             transform.Translate(Vector3.forward * 15f * Time.fixedDeltaTime, Space.World);
         }
-
+        // Mouse on the upper edge of the screen
         else if (Input.mousePosition.y >= Screen.height * 0.98) { transform.Translate(Vector3.forward * 15f * Time.fixedDeltaTime, Space.World); }
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector3.forward * 15f * -Time.fixedDeltaTime, Space.World);
         }
-
+        // Mouse on the lower edge of the screen
         else if (Input.mousePosition.y <= Screen.height * 0.02) { transform.Translate(Vector3.forward * 15f * -Time.fixedDeltaTime, Space.World); }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * 15f * Time.fixedDeltaTime, Space.World);
         }
-
+        // Mouse on the right edge of the screen
         else if (Input.mousePosition.x >= Screen.width * 0.98) { transform.Translate(Vector3.right * 15f * Time.fixedDeltaTime, Space.World); }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector3.right * 15f * -Time.fixedDeltaTime, Space.World);
         }
-
+        // Mouse on the left edge of the screen
         else if (Input.mousePosition.x <= Screen.width * 0.02) { transform.Translate(Vector3.right * 15f * -Time.fixedDeltaTime, Space.World); }  
     }
 }
