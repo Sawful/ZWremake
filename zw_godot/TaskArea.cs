b90b0fdb3b841d17bec6f5ee8ea10b2961a2eed3
Colyn Co <colynco2@gmail.com>
2024-08-30 22:02:07 +0200

    Visual effect when Task

--- zw_godot/TaskArea.cs
+++ zw_godot/TaskArea.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Numerics;
 
 public partial class TaskArea : Area3D
 {
@@ -7,17 +8,28 @@ public partial class TaskArea : Area3D
 	GameManager Main;
 	Player Player;
 	Timer ConfirmationTimer;
+	MeshInstance3D AreaIndicator;
+	Godot.Vector3 AreaSize = new();
+	bool Task;
 
 	public override void _Ready()
 	{
 		Main = GetTree().Root.GetNode<GameManager>("Main");
 		Player = Main.GetNode<Player>("Player");
 		ConfirmationTimer = GetNode<Timer>("ConfirmationTimer");
+		AreaIndicator = GetNode<MeshInstance3D>("AreaIndicator");
+		SetTask();
+	}
+
+	public override void _Process(double delta)
+	{
+		AreaSize = Godot.Vector3.Left * (float)ConfirmationTimer.TimeLeft + Godot.Vector3.Up + Godot.Vector3.Forward * (float)ConfirmationTimer.TimeLeft;
+		AreaIndicator.Scale = AreaSize;
 	}
 
 	public void OnBodyEntered(Node3D body)
 	{
-		if (body == Player)
+		if (body == Player && Task)
 		{
 			ConfirmationTimer.WaitTime = ConfirmationTime;
 			ConfirmationTimer.Start();
@@ -37,5 +49,12 @@ public partial class TaskArea : Area3D
 	{
 		Main.IncreaseThreat(1000);
 		GD.Print("Task completed");
+		Task = false;
+	}
+
+	public void SetTask()
+	{
+		ConfirmationTimer.WaitTime = 1.5f;
+		Task = true;
 	}
 }
